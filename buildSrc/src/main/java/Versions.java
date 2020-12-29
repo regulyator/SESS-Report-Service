c3e9fcbcf5d74cbb5bfc9b43200e676f028db917
Alex <regulyator777@gmail.com>
2020-12-29 17:18:56 +1000

    init project

---
+++ buildSrc/src/main/java/Versions.java
@@ -0,0 +1,7 @@
+public interface Versions {
+    String guava = "29.0-jre";
+    String apacheLang = "3.10";
+    String ronmamoReflections = "0.9.12";
+    String jupiterTest = "5.6.2";
+    String mockito = "3.3.3";
+}
---
+++ sess-report-service/src/main/java/org/sess/SESSReportService.java
@@ -0,0 +1,13 @@
+package org.sess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SESSReportService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SESSReportService.class, args);
+        System.out.println("hello");
+    }
+}
